3d6fff722ae015dd5a01f328c3803d3ade6bfac3
Nanthakorn <64312642+boomoioi@users.noreply.github.com>
2023-01-24 15:18:28 +0700

    5 again

--- lab3/5.py
+++ lab3/5.py
@@ -1,32 +1,34 @@
-def update_records(record, id, property, value):
-    if value == '':
-        del dictionary_record[id][property]
-    elif property == 'tracks':
-        if property not in dictionary_record[id].keys():
-            dictionary_record[id].update({property : [value]})
-        else:
-            dictionary_record[id][property].append(value)
-    else:
-        dictionary_record[id].update({property : value})
-
-    return dictionary_record
-
 record_collection = {
-    2548: {
-        'albumTitle' : 'Slippery When Wet',
-        'artist' : 'Bon Jovi',
-        'tracks': ['Let It Rock', 'You Give Love a Bad Name']
-    },
-    2468: {
-        'albumTitle' : '1999',
-        'artist' : 'Prince',
-        'tracks' : ['1999', 'Little Red Corvette']
-    },
-    1245: {
-        'artist' : 'Robert Palmer',
-        'tracks' : []
-    },
-    5439: {
-        'albumTitle' : 'ABBA Gold'
-    }
+  2548: {
+    'albumTitle': 'Slippery When Wet',
+    'artist': 'Bon Jovi',
+    'tracks': ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    'albumTitle': '1999',
+    'artist': 'Prince',
+    'tracks': ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    'artist': 'Robert Palmer',
+    'tracks': []
+  },
+  5439: {
+    'albumTitle': 'ABBA Gold'
+  }
 }
+
+def update_records(record, id, property, value):
+    if id not in record.keys():
+      return record
+    if value == '':
+        del record[id][property]
+    elif property == 'tracks':
+        if property not in record[id].keys():
+            record[id].update({property : [value]})
+        else:
+            record[id][property].append(value)
+    else:
+        record[id].update({property : value})
+
+    return record
